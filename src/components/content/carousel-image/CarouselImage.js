79ed6ee8c0dadd8b210eb86371871916983ab3b0
fretws <fretws@uw.edu>
2024-02-19 17:31:21 -0800

    Finish styling home page carousel and add second carousel item

--- src/components/content/carousel-image/CarouselImage.js
+++ src/components/content/carousel-image/CarouselImage.js
@@ -1,8 +1,30 @@
 import React from "react"
-import sprout from "../../../resources/img/sprout-time-lapse.png"
+import Carousel from 'react-bootstrap/Carousel'
+import styles from "./CarouselImage.module.css"
 
-export default function CarouselImage() {
+/*
+props {
+  src: Image route, (For best results, image should be 16x9)
+  alt: Image description,
+  title: Accompanying title,
+  text: Accompanying caption
+}
+ */
+
+export default function CarouselImage(props) {
   return (
-    <img src={ sprout } alt="Screenshot from personal project showing a sunset over a sprouting plant"/>
+    <>
+      <img
+        className={styles.image}
+        src={props.src}
+        alt={props.alt}
+      />
+      <Carousel.Caption>
+        <div className={styles.captionWrapper}>
+          <h3>{props.title}</h3>
+          <p>{props.children}</p>
+        </div>
+      </Carousel.Caption>
+    </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import Paragraph from '../components/text/paragraph/Paragraph'
 import Carousel from 'react-bootstrap/Carousel'
 import CarouselImage from '../components/content/carousel-image/CarouselImage'
 
+import sprout from "../resources/img/sprout-time-lapse.png"
+import cellularBetrayal from "../resources/img/social-engineering.png"
+
 export default function Home() {
   console.log("Home page rendered")
   return (
@@ -12,11 +15,30 @@ export default function Home() {
       <Paragraph>
         Welcome to my personal website, where you can see all of my proudest work in Software Development and online experiences.
       </Paragraph>
-      <Carousel>
+      <Carousel className='carousel-large'>
         <Carousel.Item>
-          <CarouselImage />
+          <CarouselImage
+            src={sprout}
+            alt="Screenshot from personal project showing a sunset over a sprouting plant"
+            title="Sprout"
+          >
+            Helping kids grow their science education and environmental awareness
+          </CarouselImage>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CarouselImage
+            src={cellularBetrayal}
+            alt="Website showing a social engineering scheme to convince the user to download a virus"
+            title="Security Vulnerabilities"
+          >
+            Understanding social engineering by exploiting a real-world vulnerability
+          </CarouselImage>
         </Carousel.Item>
       </Carousel>
+      <h2>Attributions</h2>
+      <Paragraph>
+        Cybersecurity icon by Good Wife from <a href="https://thenounproject.com/icon/cybersecurity-6535591/" target="_blank" title="Cybersecurity Icons" rel="noreferrer">Noun Project</a> (CC BY 3.0)
+      </Paragraph>
     </>
   )
 }
