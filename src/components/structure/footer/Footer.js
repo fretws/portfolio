9f686f156f09a3be7b550649d7287931917cd13c
fretws <fretws@uw.edu>
2024-02-24 17:23:43 -0800

    Ensure footer remains at bottom of the viewport when the page has minimal content

--- src/components/structure/footer/Footer.js
+++ src/components/structure/footer/Footer.js
@@ -7,23 +7,26 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     // Attributions and contact info
-    <div className={styles.footer}>
-      <div className={styles.column}>
-        <h3>Contact</h3>
-        <Stack>
-          <a href='mailto:shanefretwellbef@gmail.com'>shanefretwellbef@gmail.com</a>
-          <a href='http://www.linkedin.com/in/shane-fretwell'>Linkedin</a>
-          <a href='https://github.com/fretws'>Github</a>
-        </Stack>
+    <>
+      <div className={styles.footerSpacer} />
+      <div className={styles.footer}>
+        <div className={styles.column}>
+          <h3>Contact</h3>
+          <Stack>
+            <a href='mailto:shanefretwellbef@gmail.com'>shanefretwellbef@gmail.com</a>
+            <a href='http://www.linkedin.com/in/shane-fretwell'>Linkedin</a>
+            <a href='https://github.com/fretws'>Github</a>
+          </Stack>
+        </div>
+        <div className={styles.column}>
+          <h3>Attributions</h3>
+          <Stack>
+            <Paragraph>
+              Cybersecurity icon by Good Wife from <a href="https://thenounproject.com/icon/cybersecurity-6535591/" target="_blank" title="Cybersecurity Icons" rel="noreferrer">Noun Project</a> (CC BY 3.0)
+            </Paragraph>
+          </Stack>
+        </div>
       </div>
-      <div className={styles.column}>
-        <h3>Attributions</h3>
-        <Stack>
-          <Paragraph>
-            Cybersecurity icon by Good Wife from <a href="https://thenounproject.com/icon/cybersecurity-6535591/" target="_blank" title="Cybersecurity Icons" rel="noreferrer">Noun Project</a> (CC BY 3.0)
-          </Paragraph>
-        </Stack>
-      </div>
-    </div>
+  </>
   )
 }
